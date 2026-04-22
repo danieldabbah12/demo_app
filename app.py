48e8bf7e90ca6c940cf76c0b468d4629656a55d6
danieldabbah12 <dabbah98@gmail.com>
2026-04-22 15:14:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ st.subheader("ברוכים הבאים!")
 
 # פסקת טקסט
 st.write("שלום עולם!")
+
+st.write("טקסט רגיל")
+st.write("תומך **bold** ו-*italic* בסגנון Markdown")
+st.write(42)          # מספרים
+st.write([1, 2, 3])   # רשימות
+st.write({"a": 1})    # מילונים
